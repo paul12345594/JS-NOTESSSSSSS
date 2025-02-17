d63f853eb67f8a22e564170b2fa0260fcd43b228
unknown <dungcapaul6@gmail.com>
2025-02-18 05:59:21 +0800

    NUMBERS / OPERATORS

---
+++ Tutorial 3/three .js
@@ -0,0 +1,17 @@
+// let number = "22"
+// let number2 = 2;
+
+// number = parseInt(number);  // parseInt = string to int
+// number = parseFloat(number2);  // parseFloat = string to float
+// alert(number + number2);
+
+
+let num1 = 5;
+let num2 = 2;
+
+// Addition
+alert(num1 % num2);
+// Subtraction
+// Multiplication
+// Division
+// Modulus
