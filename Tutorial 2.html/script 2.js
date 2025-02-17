099d607c74ae292c45a91fd0b147c0d03873640f
unknown <dungcapaul6@gmail.com>
2025-02-18 03:01:43 +0800

    VARIABLE / DATA TYPES / CONCANTENATION

--- Tutorial 2.html/script 2.js
+++ Tutorial 2.html/script 2.js
@@ -1,2 +1,26 @@
-console.log("for console"); // for console
-alert("Script 2 loaded"); // for pop-up alert
+
+
+
+//CONCANTENATION // COMBINING STRINGS
+// const firstName = "Francesca";
+// alert(firstName + ", Bluen Besa");
+// alert(" hi " + firstName + " Beautifull ");
+
+// ---------------------------------------------------------------
+// const coding = true;
+// alert(coding);
+
+//---------------------------------------------------------------
+// const $name = "Edward Marquez";
+// alert($name);
+
+//---------------------------------------------------------------
+// let $name = "Paul Allen Dungca"
+// alert($name);
+
+// $name = "amira gervacio"
+// alert($name);
+
+// ---------------------------------------------------------------
+// console.log(_name); // for console
+// alert(_name); // for pop-up alert
