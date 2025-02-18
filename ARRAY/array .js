0270739f7a8fdbeb8eaee230c51d10d637956216
unknown <dungcapaul6@gmail.com>
2025-02-19 02:11:09 +0800

    Updating ARRAY / push & unshift

---
+++ ARRAY/array .js
@@ -0,0 +1,14 @@
+// let s = ["paulworx", "allenworx", "cyrilworxx", "maye"];
+// alert(s[2])
+
+// UPDATING ARRAY
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// numbers[11] = 10;
+// alert(numbers[10]);
+
+let cars = ["BMW", "Volvo", "Saab", "Ford"];
+cars.push("Toyota");
+cars.unshift("Nissan");
+
+alert(cars);
+
