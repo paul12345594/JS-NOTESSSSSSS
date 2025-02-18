b21a323973ba463fb14f9e49678a78b24fd98c9a
unknown <dungcapaul6@gmail.com>
2025-02-19 01:26:45 +0800

    () USING backticks

---
+++ Tutorial 4/four .js
@@ -0,0 +1,14 @@
+//Alenere, the friendly neighbor,
+//waved at David as she walked by David's house.
+//David smiled back invited Alenere in
+//for a cup of coffee.
+
+let s1 = "Alenere";
+let s2 = "David";
+let s3 = "she";
+let drink = "coffee";
+
+alert(`${s1}, the friendly neighbor, 
+    waved at ${s2} as ${s3} walked by ${s2}'s 
+    house. ${s2} smiled back invited ${s1} in 
+    for a cup of ${drink}.`);
