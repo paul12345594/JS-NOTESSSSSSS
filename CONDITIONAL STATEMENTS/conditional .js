8c6c657efbff8b7d1072a4514465f2a3f0904a98
unknown <dungcapaul6@gmail.com>
2025-02-19 23:44:49 +0800

    CONDITIONAL STATEMENTS / EXAMPLE

---
+++ CONDITIONAL STATEMENTS/conditional .js
@@ -0,0 +1,47 @@
+// let a = 18;
+
+// if (a >= 18) {
+//     console.log("You are eligible for voting");
+// }
+// else {
+//     console.log("sorry your're not eligible ");
+// }
+// console.log("welcome to the mobile legends");
+
+// let age = 2;
+
+// if (age >= 18) {
+//     console.log("You are eligible for voting");
+// }
+// else if (age <= 0) {
+//     console.log("You are eligible for driving");
+// }
+// else if (age >= 14) {
+//     console.log("You are eligible for facebook");
+// }
+// console.log("welcome to the mobile legends");
+
+
+// let invalid = 101;
+// let Highest = 98;
+// let High = 95;
+// let Honor = 90;
+// let Passed = 75;
+// let Failed = 74;
+let score = 99;
+
+if (score >= 101) {
+    console.log("Invalid Score");
+} else if (score >= 98 && score <= 100) {
+    console.log("Highest Honor");
+} else if (score >= 95) {
+    console.log("High Honor");
+} else if (score >= 90 && score <= 94) {
+    console.log("Honor");
+} else if (score >= 75 && score <= 89) {
+    console.log("Passed");
+} else if (score <= 74) {
+    console.log("Failed");
+    console.log("You need to study more");
+}
+
