01759b79db4871e371c71e1f0e8e59f1761b1e0f
unknown <dungcapaul6@gmail.com>
2025-02-20 01:20:31 +0800

    && || ! OPERATOR

---
+++ LOGICAL OPERATORS/logic.js
@@ -0,0 +1,19 @@
+// console.log(true && true);   // true
+// console.log(true && false);  // false
+// console.log(false && true);  // false
+// console.log(false && false); // false
+
+
+// console.log(true || true);   // true
+// console.log(true || false);  // true
+// console.log(false || true);  // true
+// console.log(false || false); // false
+
+//----------------------------------------------------------------------------
+
+
+let age = 19;
+let experience = 3;
+let language = "C++"; 
+
+alert(age >= 18 && experience < 1 || language === "C++"); //
