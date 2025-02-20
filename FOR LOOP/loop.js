4b44191af9161dbf9b64f8b1653144c84e703fd1
unknown <dungcapaul6@gmail.com>
2025-02-21 05:00:51 +0800

    EXAMPLES =  For LOOP / IN | OF | BREAK

---
+++ FOR LOOP/loop.js
@@ -0,0 +1,54 @@
+// for (variables, condition; operation) { }
+// ITERATE ARRAY
+//let people = ["David", "Alenere", "Gilbert", "James"];
+// for (let j = 0;
+//     j < people.length;
+//     j++) {
+//     console.log(people[j])  // FOR 1234
+// }
+
+// for (let j = people.length - 1;
+//     j >= 0;
+//     j--) {
+//     console.log(people[j])  // FOR 4321
+// }
+
+
+//-------------------------------------------------------------------
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// let people = ["David", "Alenere", "Gilbert", "James"];
+
+// for (let i = 0; i < people.length; i++) {
+//     console.log(people[i])
+//     if (i == 1) break;
+// }
+
+//-----------------------------------------------------------------
+// IN LOOP
+// let people = ["David", "Alenere", "Gilbert", "James"];
+
+// for (let i in people) {
+//     console.log(people[i])
+//     break;
+// }
+//-----------------------------------------------------------------
+//FOR / OF LOOP
+// let people = ["David", "Alnere", "Gilbert", "James"];
+// let i = 1;
+// for (let person of people) {
+//     console.log(`${i}. ${person}`);
+//     i++;
+// }
+// 1. David
+// 2. Alnere
+// 3. Gilbert
+// 4. James
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//REVERSE
+// let people = ["David", "Alnere", "Gilbert", "James"];
+// let i = 1;
+// for (let j = people.length - 1; j >= 0; j--) {
+//     console.log(`${i}. ${people[j]}`);
+//     i++;
+// }
+
