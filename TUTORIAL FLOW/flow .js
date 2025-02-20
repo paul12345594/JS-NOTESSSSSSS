b5ddf72280a9801c8cbceba0e430e10b270e0dae
unknown <dungcapaul6@gmail.com>
2025-02-21 01:28:36 +0800

    TUTORIAL FLOW =  WHILE LOOP / ITERATE ARRAY / BREAK KEYWORD

---
+++ TUTORIAL FLOW/flow .js
@@ -0,0 +1,37 @@
+//WHILE LOOP
+// let i = 0;
+
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+//----------------------------------------------------
+//ITERATE ARRAY
+// let names = ["David", "Alenere", "Gilbert", "James"];
+// let b = names.length - 1;
+
+// while (names[b]) {
+//     console.log(names[b]);
+//     b--;
+// }
+//----------------------------------------------------
+// BREAK KEYWORD
+let people = ["David", "Alenere", "Gilbert", "James"];
+let b = 0;
+let search = "James";  // the search only is the RESULT
+
+while (people[b]) {
+    if (people[b] === search) {
+        console.log(people[b]);
+        break;
+    }
+    b++;
+}
+
+//     //------------------------------------------------------------
+//     console.log(people[b]);
+//     b++;
+//     if (b === 3) {
+//         break;         // RESULT = "David", "Alenere", "Gilbert",
+//     }
+// }
