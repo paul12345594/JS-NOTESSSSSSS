266b1843b75063adfbc6bd111bbf4a1c2d3f014f
unknown <dungcapaul6@gmail.com>
2025-02-22 06:29:27 +0800

    JSON.parse = | STRING TO JSON FORMAT

--- JSON STRUCTURE/json STRINGIFY .js
+++ JSON STRUCTURE/json STRINGIFY .js
@@ -1,18 +1,27 @@
-let person = {
-    fistName : "David",
-    lastName: "SDPT",
-    age: 22,
-    hobbies:["coding", "Eating", "Sleeping"],   //ARRAY 
-    pets:{
-        0:{
-            name1:"DOG",
-            name2:"CAT"
-        }
-    }
-}
-console.log(person)     // 1st RUN 
+//--------------------------------------------------JSON STRINGIFYYY
 
-let paulworx = JSON.stringify(person);
-console.log(paulworx);  // 2ND RUN = STRING FORMAT ------- {"fistName":"David","lastName":"SDPT","age":22}.
-                        // JSON.stringify  =  CONVERT TO STRING FORMAT 
+// let person = {
+//     fistName : "David",
+//     lastName: "SDPT",
+//     age: 22,
+//     hobbies:["coding", "Eating", "Sleeping"],   //ARRAY 
+//     pets:{
+//         0:{
+//             name1:"DOG",
+//             name2:"CAT"
+//         }
+//     }
+// }
+// console.log(person)     // 1st RUN 
+
+// let paulworx = JSON.stringify(person);
+// console.log(paulworx);  // 2ND RUN = STRING FORMAT ------- {"fistName":"David","lastName":"SDPT","age":22}.
+//                         // JSON.stringify  =  CONVERT TO STRING FORMAT 
+
+
+//--------------------------------------------------JSON.parse
+// let strPerson = `{"firstName":"David", "lastName":"Dungca", "age":"22"}`;
+
+// let person = JSON.parse(strPerson);         // STRING TO JSON FORMAT
+// console.log(person);
 
