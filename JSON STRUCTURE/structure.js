32d1a0470081b8dcf6ea579f3b071addc61111cc
unknown <dungcapaul6@gmail.com>
2025-02-22 05:35:27 +0800

    JSON WRITE = | UPDATE EXISTING | CREATE NEW | ARRAY UPDATE |

--- JSON STRUCTURE/structure.js
+++ JSON STRUCTURE/structure.js
@@ -1,29 +1,77 @@
-let person = {
-    name: "paulworx",
-    surname: "dungca",
-    age: 22,
-    hobbies: ["basketball", "vollyeball", "long-run"],
 
-    pets:{
-        0:{
-            name:"DOG",
-            breed:"ASKALS"
-        },
-        1:{
-            name:"CAT",
-            breed:"PERSIAN"
-        }
-    },
-    foods:{
-        0:{
-            name:"BURGER",
-            calories:22
-        },
-        1:{
-            name:"FOOTLONG",
-            calories:32
-        }
-    }
+// JSON READ 
+// let person = {
+//     name: "paulworx",
+//     lastname: "dungca",
+//     age: 22,
+//     bloodtype:"OB",
+//     hobbies:["coding", "Eating", "Sleeping"], // ARRAY = INDEX
 
-}
-console.log(person);
+//     pets:{
+//         0:{
+//             name:"DOG",
+//             breed:"ASKALS"
+//         },
+//         1:{
+//             name:"CAT",
+//             breed:"PERSIAN"
+//         }
+//     },
+//     foods:{
+//         0:{
+//             name:"BURGER",
+//             calories:22
+//         },
+//         1:{
+//             name:"FOOTLONG",
+//             calories:32
+//         }
+//     },
+//     barangay:{
+//         0:{
+//             name:"LARA"
+//         },
+//         1:{
+//             name:"BALITI"
+//         },
+//         2:{
+//             name:"SAN ISIDRO"
+//         },
+//         3:{
+//             name:"SAGUIN"
+//         }
+//     }
+// }
+//                                          JSON READ 
+// console.log(person.name);
+// console.log(person["age"]);
+// console.log(`${person.bloodtype} ${person.lastname}`)  //COMBINE 2 SINGLE LINE
+
+// console.log(person["hobbies"][2]);  // ARRAY
+// console.log(person.hobbies[0]); // ARRAY
+
+// console.log(person["barangay"][0]["name"]);      //LONG FORM = using BRACKET 
+// console.log(person.barangay[1].name)            // LONG FORM = NOTATION 
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+// let person = {
+//     firstName: "Paulworx",
+//     lastName:"SDPT",
+//     age: 23,
+//     hobbies:["coding", "Eating", "Sleeping"]   //ARRAY 
+// };
+//console.log(person); ----------------------// 1st RUN before the UPDATE EXISTING
+
+// // UPDATE EXISTING "key|value"
+// person["firstName"] = "Paul Allen"
+// console.log(person)
+
+// //CREATE NEW "key|value"
+// person["MiddleInitial"] = "C"
+// console.log(person)
+
+//console.log(person.hobbies);-----------------------   // 1st RUN 
+//person.hobbies[0] = "Playing";    // ARRAY UPDATE EXISTING 
+//console.log(person.hobbies)
+
+
