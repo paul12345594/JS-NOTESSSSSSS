aac056e4143f1bdd2a0267a9faa08378e6b50728
unknown <dungcapaul6@gmail.com>
2025-02-22 01:31:24 +0800

    JSON simple STRUCTURE

---
+++ JSON STRUCTURE/structure.js
@@ -0,0 +1,29 @@
+let person = {
+    name: "paulworx",
+    surname: "dungca",
+    age: 22,
+    hobbies: ["basketball", "vollyeball", "long-run"],
+
+    pets:{
+        0:{
+            name:"DOG",
+            breed:"ASKALS"
+        },
+        1:{
+            name:"CAT",
+            breed:"PERSIAN"
+        }
+    },
+    foods:{
+        0:{
+            name:"BURGER",
+            calories:22
+        },
+        1:{
+            name:"FOOTLONG",
+            calories:32
+        }
+    }
+
+}
+console.log(person);
